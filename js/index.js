cea76d4a91d71f3eb8f12996907f35504b920aad
yoonchungah <yca3325@gmail.com>
2023-02-01 03:35:24 +0900

    반응형 수정

--- js/index.js
+++ js/index.js
@@ -73,6 +73,9 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   },
   navigation: {
     nextEl: ".swiper-button-next",
